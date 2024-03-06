a7fca89cda64530c3315e3fe3fb3ac4ac5b606a0
mohamed <mohameddrame907@gmail.com>
2024-03-06 11:55:05 +0100

    feat/add default icon

--- src/Components/TabNavigator.js
+++ src/Components/TabNavigator.js
@@ -1,4 +1,4 @@
-import React, {useCallback, Image} from 'react';
+import React, {useCallback} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Catch from './Catch';
 import Library from './Library';
@@ -13,16 +13,35 @@ const TabNavigator = ({route}) => {
       <Tab.Screen
         name="Library"
         component={Library}
-
+        options={({}) => ({
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={30} color="#900" />
+          ),
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
       />
       <Tab.Screen
         name="Catch"
         component={Catch}
+        options={({}) => ({
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" size={30} color="#900" />
+          ),
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
       />
       <Tab.Screen
         name="Profil"
         component={Profil}
-
+        options={({}) => ({
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" size={30} color="#900" />
+          ),
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray',
+        })}
       />
     </Tab.Navigator>
   );
